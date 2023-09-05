daea6e19c2d2123da50f46f1e67400b96f27a639
Toan Le <toanlee.dev@gmail.com>
2023-09-05 17:53:43 +0700

    Init project

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    adaptiveHeight: true
+  });
+});
